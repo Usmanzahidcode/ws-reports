9a2bf16ef5e98b668e92b819812d8118e7c17532
Usman Zahid <developerusman@yahoo.com>
2025-03-24 21:30:44 +0500

    added bases for the registration form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,25 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
+
+// Homepage
 Route::get('/', fn() => view('homepage'));
 
+// Auth Routes
+Route::group(['as' => 'auth.', 'prefix' => 'auth'], function () {
+    Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+
+    Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout.submit');
+
+//    Route::get('/forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('forgot-password');
+//    Route::post('/forgot-password', [AuthController::class, 'sendResetLink']);
+//
+//    Route::get('/reset-password/{token}', [AuthController::class, 'showResetPasswordForm'])->name('reset-password');
+//    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+});
